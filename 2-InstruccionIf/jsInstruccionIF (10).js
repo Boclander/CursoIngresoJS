1bcd3022d4595595be269a123ec46b262d48a61b
Boclander <Boclander@gmail.com>
2018-07-10 12:26:13 -0300

    Unidad 2
    Ejecicio 10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,17 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var rand=Math.floor(Math.random()*(11-1))+1;
 	
+	if(rand >= 9){
+		alert("EXCELENTE");
+	} else {
+		if(rand >=4 && rand < 9){
+			alert("APROBO");
+		} else{
+			alert("Vamos, la proxima se puede")
+		}
+	}
+
 
 }//FIN DE LA FUNCIÓN
