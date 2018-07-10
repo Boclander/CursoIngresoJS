d9a91d018d4ae020e865bb9a15ad4d90022394c3
Boclander <Boclander@gmail.com>
2018-07-10 10:59:09 -0300

    If 1 y 2

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,22 @@
 function mostrar()
 {
+    var precio;
+    var descuento;
+    var descuentoEnDinero;
+    var precioConDescuento;
+    var iva;
+    var precioFinal;
 
+    precio=prompt("Precio");
+    descuento=prompt("Descuento");
+    precio=parseInt(precio);
+    descuento=parseInt(descuento);
+
+    descuentoEnDinero=descuento*precio/100;
+    precioConDescuento=precio-descuentoEnDinero;
+    iva=precioConDescuento*0.21;
+    precioFinal=precioConDescuento+iva;
+
+    alert("1. El descuento en dinero es de "+descuentoEnDinero+" // 2. El precio con descuento es de "+precioConDescuento+" // 3. El IVA es de "+iva);
+    document.getElementById("elPrecioFinal").value=precioFinal;
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+    var base;
+    var altura;
+    var superficie;
+    var perimetro;
 
+    base=prompt("Base");
+    altura=prompt("Altura");
+    base=parseInt(base);
+    altura=parseInt(altura);
+
+    perimetro=base*3;
+    superficie=base*altura/2;
+    alert("El perimetro es "+perimetro+" y la superficie es "+superficie);
 }
