e2e2ca1519309592b55f7d2aea6380a4cf8bd6fb
Boclander <Boclander@gmail.com>
2018-07-10 12:18:44 -0300

    Unidad 2
    Ejercicios -10

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad;
+    edad=document.getElementById("edad").value;
 
+    if(edad >17 || edad <13) {   //es lo mismo que hacer el ejercicio anterior poniendole !=
+        alert("Usted no es un adolescente.");
+    }
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,9 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var rand;
+	rand=Math.floor(Math.random()*(11-1))+1
+
+	alert(rand);
 
 }//FIN DE LA FUNCIÓN
