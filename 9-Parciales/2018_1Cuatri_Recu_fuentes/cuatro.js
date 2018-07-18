81461a6f5d86d90966a5183b4130bbab57ff43f1
Boclander <Boclander@gmail.com>
2018-07-17 22:01:57 -0300

    Recuperatorio -4

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
+    var numeroUno;
+    var numeroDos;
+    var suma;
 
+    numeroUno=prompt("Ingrese el primer número");
+    numeroDos=prompt("Ingrese el segundo número");
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+    suma=numeroUno+numeroDos
+
+    if (numeroUno == numeroDos){
+        alert(numeroUno+" "+numeroDos);
+    } else if (numeroUno > numeroDos){
+        alert(numeroUno/numeroDos);
+    } else {
+        alert(numeroUno+numeroDos);
+    }
+    if (numeroDos > numeroUno && numeroUno+numeroDos < 50){
+        alert("La suma es "+suma+" y es menor a 50.");
+    }
 }
