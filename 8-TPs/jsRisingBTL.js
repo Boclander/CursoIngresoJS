05e455f45b6736f35e2d09ec12fbf09a56a73830
Boclander <Boclander@gmail.com>
2018-07-26 12:08:16 -0300

    TP 8

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,76 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+    var edad;
+    var sexo;
+    var estadoCivil;
+    var sueldo;
+    var legajo;
+    var nacionalidad;
+
+    edad=prompt("Ingrese edad.");
+    edad=parseInt(edad);
+    while (edad < 17 || edad > 90){
+        edad=prompt("Ingrese edad.");
+    }
+    sexo=prompt("Ingrese sexo (M o F).");
+    switch (sexo){
+        case "M":
+        case "m":
+            sexo="Masculino";
+        break;
+        case "F":
+        case "f":
+            sexo="Femenino";
+        break;
+        default:
+        sexo=prompt("Ingrese sexo (M o F).");
+    }
+    estadoCivil=prompt("Ingrese estado civil: 1-para soltero, 2-para casado, 3-para divorciado o 4-para viudo.");
+    switch (estadoCivil){
+        case "1":
+            estadoCivil="Soltero";
+        break;
+        case "2":
+            estadoCivil="Casado";
+        break;
+        case "3":
+            estadoCivil="Divorciado";
+        break;
+        case "4":
+            estadoCivil="Viudo";
+        break;
+        default:
+            estadoCivil=prompt("Ingrese estado civil: 1-para soltero, 2-para casado, 3-para divorciado o 4-para viudo.");
+    }
+    sueldo=prompt("Ingrese sueldo bruto.");
+    sueldo=parseInt(sueldo);
+    while (sueldo <= 8000){
+        sueldo=prompt("Ingrese sueldo bruto.");
+    }
+    legajo=prompt("Ingrese número de legajo (4 cifras, sin ceros a la izquierda).");
+    legajo=parseInt(legajo);
+    while (legajo < 1000 || legajo > 9999){
+        legajo=prompt("Ingrese número de legajo (4 cifras, sin ceros a la izquierda).");
+    }
+    nacionalidad=prompt("Ingrese nacionalidad (A para Argentinos, E para Extranjeros y N para Nacionalizados).");
+    switch (nacionalidad){
+        case "A": 
+            nacionalidad="Argentino";
+        break;
+        case "E":
+            nacionalidad="Extranjero";
+        break;
+        case "N":
+            nacionalidad="Nacionalizado";
+        break;
+        default:
+        nacionalidad=prompt("Ingrese nacionalidad (A para Argentinos, E para Extranjeros y N para Nacionalizados).");
+    }
+    document.getElementById("Edad").value=edad;
+    document.getElementById("Sexo").value=sexo;
+    document.getElementById("EstadoCivil").value=estadoCivil;
+    document.getElementById("Sueldo").value=sueldo;
+    document.getElementById("Legajo").value=legajo;
+    document.getElementById("Nacionalidad").value=nacionalidad;
 }
