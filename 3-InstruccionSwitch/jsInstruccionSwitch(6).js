6f9149fa9b0eefff55c1603dadc36d7649c3f9c4
Boclander <Boclander@gmail.com>
2018-07-26 10:42:07 -0300

    Switch terminado

--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -8,21 +8,15 @@ laHora=parseInt(laHora);
         case laHora:
             if (laHora >= 7 && laHora <= 11){
                 alert("Es de mañana.");
-            }
-        break;
-        case laHora:
-            if (laHora >= 12 && laHora <= 19){
+            } else if (laHora >= 12 && laHora <= 19){
                 alert("Es de tarde.");
-            }
-        break;
-        case laHora:
-            if ((laHora >= 20 && laHora <= 24) || (laHora >= 0 && laHora <= 6)){
+            } else  if ((laHora >= 20 && laHora <= 24) || (laHora >= 0 && laHora <= 6)){
                 alert("Es de noche.");
+            } else {
+                alert("No existe la hora.");
             }
         break;
-        default:
-            alert("No existe la hora");
-        break;
-}
+            
+            }
 }
 //FIN DE LA FUNCIÓN
