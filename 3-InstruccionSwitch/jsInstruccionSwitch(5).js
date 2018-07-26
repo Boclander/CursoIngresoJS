1a395ef9235e9213ba3ce9c199eae399777f76cf
Boclander <Boclander@gmail.com>
2018-07-26 10:37:12 -0300

    Switch
    Error ejercicio 6

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -2,9 +2,15 @@ function mostrar()
 {
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
-
+laHora=parseInt(laHora);
 //alert (laHora);
-	
+	switch (laHora){
+        case laHora:
+            if (laHora >= 7 && laHora <= 11){
+            alert("Es de mañana.")
+        }
+        break;
+    }
 	
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -2,7 +2,27 @@ function mostrar()
 {
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
+laHora=parseInt(laHora);
 
-
-
-}//FIN DE LA FUNCIÓN
+    switch (laHora){
+        case laHora:
+            if (laHora >= 7 && laHora <= 11){
+                alert("Es de mañana.");
+            }
+        break;
+        case laHora:
+            if (laHora >= 12 && laHora <= 19){
+                alert("Es de tarde.");
+            }
+        break;
+        case laHora:
+            if ((laHora >= 20 && laHora <= 24) || (laHora >= 0 && laHora <= 6)){
+                alert("Es de noche.");
+            }
+        break;
+        default:
+            alert("No existe la hora");
+        break;
+}
+}
+//FIN DE LA FUNCIÓN
